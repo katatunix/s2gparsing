6101fa51803cf41cff986ef4f6c06d1858a2cfdf
katatunix <katatunix@gmail.com>
2016-11-04 14:59:19 +0700

    win32 project: support reading .props files

--- src/com/nghiabui/s2gparsing/win32/Win32Project.java
+++ src/com/nghiabui/s2gparsing/win32/Win32Project.java
@@ -1,7 +1,9 @@
 package com.nghiabui.s2gparsing.win32;
 
 import com.nghiabui.kommon.AppException;
+import com.nghiabui.kommon.ListOperation;
 import com.nghiabui.kommon.Path;
+import com.nghiabui.kommon.xml.DocumentFactory;
 import com.nghiabui.kommon.xml.NodeUtil;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -9,6 +11,7 @@ import org.w3c.dom.Node;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -27,15 +30,43 @@ public class Win32Project {
 	public Path folderPath() {
 		return projFolder;
 	}
-
+	
 	public List<Path> includePaths(String configName) {
-		return NodeUtil.toList(document.getElementsByTagName("AdditionalIncludeDirectories")).stream()
+		final List<Element> mainElements = aidElements(document).stream()
 			.filter(e -> {
 				final Node parent = e.getParentNode();
 				if (parent == null) return false;
 				final Node grand = parent.getParentNode();
 				return grand != null && NodeUtil.isElement(grand) && conditionPassed((Element) grand, configName);
+			}).collect(Collectors.toList());
+		
+		final List<Element> externalElements = NodeUtil.toList(document.getElementsByTagName("ImportGroup")).stream()
+			.filter(e -> "PropertySheets".equals(e.getAttribute("Label")) && conditionPassed(e, configName))
+			.flatMap(e -> NodeUtil.toList(e.getChildNodes()).stream())
+			.filter(e -> {
+				if (!e.getTagName().equals("Import")) return false;
+				final String project = e.getAttribute("Project");
+				return project != null && !project.contains("$");
 			})
+			.flatMap(e -> {
+				final Path propsFilePath = projFolder.combination(e.getAttribute("Project"));
+				final Optional<Document> op = DocumentFactory.createDocument(propsFilePath);
+				if (!op.isPresent()) {
+					throw new AppException("Could not read and parse: \"" + propsFilePath.absolute() + "\"");
+				}
+				return aidElements(op.get()).stream();
+			})
+			.collect(Collectors.toList());
+		
+		return handle_AID_Elements(ListOperation.concat(mainElements, externalElements));
+	}
+	
+	private static List<Element> aidElements(Document doc) {
+		return NodeUtil.toList(doc.getElementsByTagName("AdditionalIncludeDirectories"));
+	}
+	
+	private List<Path> handle_AID_Elements(List<Element> elements) {
+		return elements.stream()
 			.map(Node::getTextContent)
 			.flatMap(paths -> Arrays.stream(paths.split(";")))
 			.filter(path -> !path.contains("%"))
@@ -73,7 +104,7 @@ public class Win32Project {
 			.collect(Collectors.toList());
 	}
 
-	private boolean conditionPassed(Element element, String configName) {
+	private static boolean conditionPassed(Element element, String configName) {
 		final String condition = element.getAttribute("Condition");
 		if (condition.isEmpty()) return false;
 		if (!condition.startsWith("'$(Configuration)|$(Platform)'==")) {
